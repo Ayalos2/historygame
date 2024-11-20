2c3cab75f13e8a16a7bb7941b3846bc42688bf1a
Lev1Ntc <164568985+Lev1Ntc@users.noreply.github.com>
2024-11-19 21:00:00 -0300

    Pagina SOBRE
    Atualização da pagina SOBRE

--- historygame/src/views/SobrePage.vue
+++ historygame/src/views/SobrePage.vue
@@ -1,6 +1,8 @@
 <template>
 
-    <h1>Sobre</h1>
+    <h1>Este site é destinado a pessoas apaixonadas por video-games assim como nós, desenvolvemos este site para aproximação de comunidades de jogos distindos e de gerações diferentes. 
+Queremos trazer o sentimento de pertencimento a um mundo imerso nos Games onde cada um pode trazer suas experiências e opniões sobre o Game que você desejar, e ainda ajudar aqueles
+que estão a procura de opniões sobre a obra que deseja jogar. Esperamos que divirtam-se em nosso Site.</h1>
 
 </template>
 
