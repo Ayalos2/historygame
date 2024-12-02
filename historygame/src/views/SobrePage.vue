3fcab4490e7667b8f76c4efa66eaaf4c18de2386
Lev1Ntc <164568985+Lev1Ntc@users.noreply.github.com>
2024-12-01 22:26:38 -0300

    Atualização da pagina Sobre
    Correção de erros de ortografia

--- historygame/src/views/SobrePage.vue
+++ historygame/src/views/SobrePage.vue
@@ -1,8 +1,12 @@
 <template>
 
-    <h1>Este site é destinado a pessoas apaixonadas por video-games assim como nós, desenvolvemos este site para aproximação de comunidades de jogos distindos e de gerações diferentes. 
-Queremos trazer o sentimento de pertencimento a um mundo imerso nos Games onde cada um pode trazer suas experiências e opniões sobre o Game que você desejar, e ainda ajudar aqueles
-que estão a procura de opniões sobre a obra que deseja jogar. Esperamos que divirtam-se em nosso Site.</h1>
+    <h1>Este site foi criado para pessoas apaixonadas por videogames, assim como nós! Nosso objetivo é conectar comunidades de jogadores de diferentes gerações e estilos de jogo, promovendo interação e troca de experiências.
+
+Queremos proporcionar um espaço onde você possa compartilhar suas opiniões e vivências sobre os games que ama, além de ajudar outros jogadores que estão em busca de recomendações para decidir qual aventura explorar a seguir.
+
+Esperamos que você se sinta parte de um universo vibrante e imersivo, cheio de histórias e conexões. Divirta-se e aproveite ao máximo sua experiência em nosso site!
+
+</h1>
 
 </template>
 
