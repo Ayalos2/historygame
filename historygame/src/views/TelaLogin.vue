9f288025a326ebab6455fc3bb478cc30ed5dec3a
Reginaldo Ayalos <ras45@discente.ifpe.edu.br>
2024-12-06 23:11:37 -0300

    Removido CSS
    Removido a tela de login do CSS

--- historygame/src/views/TelaLogin.vue
+++ historygame/src/views/TelaLogin.vue
@@ -13,7 +13,7 @@
         <br><br>
         <button>Log in</button>
         <br><br>
-        <p3><a href="TelaRegistro.html">Esqueceu a Senha? </a></p3>
+        <p3><a>Esqueceu a Senha? </a></p3>
 
         
 
@@ -28,41 +28,3 @@
 
 </script>
 
-<style>   
-body{
-font-family: Arial, Helvetica, sans-serif;
-background-image : white
-}
-div{
-    background-color: black;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 80px;
-    border-radius: 15px;
-    color: white;
-
-}
-input{
-    padding: 15px;
-    border: none;
-    outline: none;
-    font-size: 15px;
-}   
-
-button{
-    background-color: white;
-    border: none;
-    padding: 15px;
-    white-space: 100%;
-    width: 100%;
-    border-radius: 10px;
-}
-button:hover{
-    background-color: gray;
-    cursor: pointer;
-
-} 
-
-</style>
